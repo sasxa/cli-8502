ef9bc2c8cb949cd908766c3f1736adb5a92ef01d
Aleksandar Milošević <dev@sasxa.net>
2017-11-15 19:20:28 +0100

    working

---
+++ one/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <!--The content below is only a placeholder and can be replaced.-->
+  `,
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'app';
+}
---
+++ two/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <!--The content below is only a placeholder and can be replaced.-->
+    <app-test></app-test>
+  `,
+  styleUrls: ['./app.component.styl']
+})
+export class AppComponent {
+  title = 'app';
+}
